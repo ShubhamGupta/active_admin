77adc47c6e6ad61f42c661df22f0e2147eae4b02
shubham gupta <shubham.gupta@vinsol.com>
2013-07-07 23:37:23 +0530

    Determining namespace from config file for creating resource directory

--- lib/generators/active_admin/resource/resource_generator.rb
+++ lib/generators/active_admin/resource/resource_generator.rb
@@ -8,7 +8,18 @@ module ActiveAdmin
       end
 
       def generate_config_file
-        template "admin.rb", "app/sing/#{file_path.gsub('/', '_').pluralize}.rb"
+        file = File.open('config/initializers/active_admin.rb')
+        namespace_line = ""
+        file.each_line do |line|
+          namespace_line = line and break if !line.index('#') && line =~ /config.default_namespace/
+        end
+        # If namespace is not defined in active_admin.rb, use app/admin
+        if namespace_line.strip!.blank?
+          template "admin.rb", "app/admin/#{file_path.gsub('/', '_').pluralize}.rb"
+        else
+          new_namespace = namespace_line.split(' ')[-1].delete(":")
+          template "admin.rb", "app/#{new_namespace}/#{file_path.gsub('/', '_').pluralize}.rb"
+        end
       end
 
     end
